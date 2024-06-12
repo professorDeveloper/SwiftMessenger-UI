b28df16218530f65cb0e9b2553fe3b527b2d3287
saikou <azamovhud007@gmail.com>
2024-06-12 11:49:12 +0500

    Initial Commit

---
+++ Soplay/SoplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoplayApp.swift
+//  Soplay
+//
+//  Created by saikou on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SoplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
