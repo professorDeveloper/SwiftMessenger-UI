a7b0ed789873df85efb1d90e7839986b8ba6d12e
saikou <azamovhud007@gmail.com>
2024-06-19 18:34:00 +0500

    Init Project

---
+++ Soplay/Model/Tab.swift
@@ -0,0 +1,15 @@
+//
+//  Tab.swift
+//  Soplay
+//
+//  Created by saikou on 6/19/24.
+//
+
+import SwiftUI
+
+enum Tab :String,CaseIterable {
+case all = "All"
+case personal = "Personal"
+case office = "Updates"
+case community = "Gaming"
+}
